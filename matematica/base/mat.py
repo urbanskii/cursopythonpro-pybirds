147719dd13653f9c2c82aec484e47e2e560051b9
marcos <marcos.urbanski@gmail.com>
2020-04-14 17:34:29 -0300

    close #5

--- matematica/base/mat.py
+++ matematica/base/mat.py
@@ -1,4 +1,12 @@
+"Módulo que contém operações Matemáticas"
 def soma(parcela, parcela_2):
+    """Essa função calcula a soma de duas parcelas
+
+    :param parcela: number
+    :param parcela_2: number
+    :return: number
+    """
+
     return parcela + parcela_2
 
 if  __name__ == '__main__':
